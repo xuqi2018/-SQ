06bee0c4e92987ba83110cc78a39e5fae611e2f6
WilliamLeaves <Williamtheleaves@gmail.com>
2018-12-15 22:15:42 +0800

    12-15 from William
    今天早中晚都有课，所以只写了mainStage关于文本显示的部分，以及刷新请求数据的功能

--- pages/mainStage/mainStage.js
+++ pages/mainStage/mainStage.js
@@ -4,63 +4,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    feed:[],
+    feed_length:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    var that =this
+    this.getData();
   },
   upper: function () {
     wx.showNavigationBarLoading()
@@ -73,5 +26,24 @@ Page({
     var that = this;
     //setTimeout(function () { wx.hideNavigationBarLoading(); that.nextLoad(); }, 1000);
     console.log("lower")
+  },
+  //事件处理函数,暂时没有添加问题回答的id的传输
+  bindItemTap: function () {
+    wx.navigateTo({
+      url: '../answer/answer'
+    })
+  },
+  bindQueTap: function () {
+    wx.navigateTo({
+      url: '../question/question'
+    })
+  },
+  getData:function(){
+    wx.request({
+      url: 'localhost:8000/getQuestion',
+      seccess: function(res){
+
+      }
+    })
   }
 })
